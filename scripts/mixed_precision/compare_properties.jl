aae2b878e371d79281f8cec23d473ba4f28ea7d2
Connor Fundell <cfundell@iastate.edu>
2025-06-04 08:12:49 -0700

    First push

---
+++ scripts/mixed_precision/compare_properties.jl
@@ -0,0 +1,74 @@
+include("/global/cfs/cdirs/m4265/cfundell/JuliaChem.jl/example_scripts/jc_timings_read.jl") # JuliaChem/example_scripts/jc_timings_read.jl
+include("/global/cfs/cdirs/m4265/cfundell/JuliaChem.jl/example_scripts/Scripts/input_output_file_helper.jl")
+using HDF5
+ 
+function get_run_scf_energies(path_to_outputs)
+    total_SCF_Energies = Dict{String, Float64}() # Energies for the various input file
+   
+    #files at path
+    run_folders, full_folder_paths, full_folder_paths = get_output_folders(path_to_outputs)
+    for run_folder_path in full_folder_paths
+        files = readdir(run_folder_path)
+        full_file_paths = [joinpath(run_folder_path, file) for file in files]
+ 
+        run_name = split(run_folder_path, "/")[end]
+        input_SCF_Energies = []
+ 
+        for file_path in full_file_paths #copies for each Run and MPI RANK?
+            run_level_data, timings, scf_options, scf_options_user, non_timing_data = jc_timings_read(file_path)
+            #create dictionary from Nx2 array
+            run_level_data_dict = Dict(run_level_data[:, 1] .=> run_level_data[:, 2])
+            push!(input_SCF_Energies, parse(Float64, run_level_data_dict["scf_energy"])) # Assuming the first row contains the SCF energy
+        end
+        total_SCF_Energies[run_name] = input_SCF_Energies[1] # Store the first SCF energy value for each run configuration
+    end
+    return total_SCF_Energies
+end
+ 
+ 
+ 
+function main(paths_to_outputs)
+   
+    scf_energies_for_run_type = Array{Dict{String, Float64}}(undef, length(paths_to_outputs))
+ 
+    if length(paths_to_outputs) > 2
+        error("This script is designed to handle only two paths to outputs at the moment.")
+    end
+ 
+    i = 1
+    for path in paths_to_outputs
+        println("path: $path")
+        folder_scf_energies = get_run_scf_energies(path)
+        scf_energies_for_run_type[i] = folder_scf_energies
+        i += 1
+    end
+ 
+    max_diff = -100000.0
+    max_diff_input = ""
+    input_file_keys = keys(scf_energies_for_run_type[1])
+    for input_key in input_file_keys
+        scf_energy_1 = scf_energies_for_run_type[1][input_key]
+        scf_energy_2 = scf_energies_for_run_type[2][input_key]
+        diff = abs(scf_energy_1 - scf_energy_2)
+        println("SCF ΔE for $input_key: | $(scf_energy_1) - $(scf_energy_2) | = $diff")
+        if diff > max_diff
+            max_diff = diff
+            max_diff_input = input_key
+        end
+    end
+    println("Maximum difference in SCF energies: $max_diff")
+ 
+ 
+    
+ 
+    # display(scf_energies_for_run_type)
+end
+ 
+# path1 =  "/pscratch/sd/j/jhayes1/source/JuliaChem.jl/JuliaChem-Papers/DF-RHF-Paper/Benchmarks/0.4.3/1hsg/perlmutter/DF_RHF_screenedCPU"
+# path2 =  "/pscratch/sd/j/jhayes1/source/JuliaChem.jl/JuliaChem-Papers/DF-RHF-Paper/Benchmarks/0.4.3/1hsg/perlmutter/DF_RHF_screenedCPU_no_sym"
+# main([path1, path2])
+
+path1="/global/cfs/cdirs/m4265/cfundell/JuliaChem.jl/Benchmarks/scripts/mixed_precision/S22/DF_RHF_denseCPU_mixed_6-311++G_2d_2p__divide10"
+path2="/global/cfs/cdirs/m4265/cfundell/JuliaChem.jl/Benchmarks/scripts/mixed_precision/S22/DF_RHF_denseCPU_DOUBLE_6-311" 
+
+main([path1, path2]) 
--- scripts/mixed_precision/run.sh
+++ scripts/mixed_precision/run.sh
@@ -8,7 +8,7 @@ module load julia/1.11.4
 
 # export PATH=/pscratch/sd/j/jhayes1/software/julia-1.11.4/bin/:$PATH
 
-export JC_PATH=/global/cfs/cdirs/m4265/mixed_precision/JuliaChem.jl
+export JC_PATH=/global/cfs/cdirs/m4265/cfundell/JuliaChem.jl/
 
 export script_path=$JC_PATH/Benchmarks/scripts/mixed_precision
 
@@ -27,32 +27,36 @@ export JULIA_NUM_THREADS=$threads_per_socket
 export input_file_path=$JC_PATH/example_inputs/S22
 
 
-export basis_id="cc-pvdz-ri"
-# export basis_id="cc-pvdz-jk"
-# export basis_id="6-311++G_2d_2p_"
+#export basis_id="cc-pvdz-ri"
+#export basis_id="cc-pvdz-jk"
+export basis_id="6-311++G_2d_2p_"
 export run_start_index=1
 export number_of_runs=1
 export file_start_index=1
-export file_end_index=5
-export settings_id=DF_RHF_denseCPU
+export file_end_index=1
+export settings_id=DF_RHF_denseCPU_mixed
 
-export output_full_path=$script_path/S22/${settings_id}_MIXED
-mkdir -p $output_full_path
+# export output_full_path=$script_path/S22/${settings_id}_${basis_id}_Qtest
+# rm -rf $output_full_path
+# mkdir -p $output_full_path
 
-export DO_MIXED=true
+ #export DO_MIXED=true
 
 # srun -n 1  --cpu-bind=socket --cpu-bind=v 
-julia --sysimage=$JULIACHEM_SYSIMG_PATH \
- --project=$j_project_path --threads=$threads_per_socket --check-bounds=no --math-mode=fast --optimize=3 --inline=yes \
- $run_script $input_file_path $output_full_path $settings_id $basis_id \
-  $run_start_index $number_of_runs $file_start_index $file_end_index \
-  &>> $output_full_path/output.log
-
-# export DO_MIXED=false
-# export output_full_path=$script_path/S22/${settings_id}_DOUBLE
-
-# julia --sysimage=$JULIACHEM_SYSIMG_PATH \
+#julia --sysimage=$JULIACHEM_SYSIMG_PATH \
+# julia \
 #  --project=$j_project_path --threads=$threads_per_socket --check-bounds=no --math-mode=fast --optimize=3 --inline=yes \
 #  $run_script $input_file_path $output_full_path $settings_id $basis_id \
 #   $run_start_index $number_of_runs $file_start_index $file_end_index \
 #   &>> $output_full_path/output.log
+
+export DO_MIXED=true
+export output_full_path=$script_path/S22/${settings_id}_${basis_id}_timingtest
+mkdir -p $output_full_path
+
+# julia --sysimage=$JULIACHEM_SYSIMG_PATH \
+julia \
+ --project=$j_project_path --threads=$threads_per_socket --check-bounds=no --math-mode=fast --optimize=3 --inline=yes \
+ $run_script $input_file_path $output_full_path $settings_id $basis_id \
+  $run_start_index $number_of_runs $file_start_index $file_end_index \
+  &> $output_full_path/output.log
